fb5aaa503326f0c7252f278ef71dc207ccd11963
Aron <aron.cachago@gmail.com>
2023-05-24 20:30:37 -0500

    "Mejorar eficencia del cosigo"

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("usuarios").addEventListener("click", cargarIframe, false);
     document.getElementById("catalogo").addEventListener("click", cargarIframe, false);
     document.getElementById("tienda").addEventListener("click", cargarIframe, false);
-    var iframe = document.getElementById("iframe_panel");
+    const iframe = document.getElementById("iframe_panel");
 
     function cargarIframe(e) {
         switch (e.target) {
--- js/usuarios.js
+++ js/usuarios.js
@@ -12,19 +12,19 @@ function ObetnerTabla() {
             let tablaHtml = document.getElementById("tabla");
             modalId.readOnly = true;
             let arrayDataUsuario = [];
-            let tabla = "";
+            let tabla = ``;
 
-            
-            tabla += "<table class=\"table\"><thead>  <tr>    <th scope=\"col\">ID</th>    <th scope=\"col\">Nombre</th>    <th scope=\"col\">Usuario</th>    <th scope=\"col\">Nivel</th> <th></th> <th></th> </tr></thead><tbody>";
+
+            tabla += `<table class=\"table\"><thead>  <tr>    <th scope=\"col\">ID</th>    <th scope=\"col\">Nombre</th>    <th scope=\"col\">Usuario</th>    <th scope=\"col\">Nivel</th> <th></th> <th></th> </tr></thead><tbody>`;
             for (let i = 0; i < data.length; i++) {
-                tabla += "<tr>";
-                tabla += `<td>${data[i].id}</td>`;
-                tabla += `<td>${data[i].nombre}</td>`;
-                tabla += `<td>${data[i].username}</td>`;
-                tabla += `<td>${data[i].nivel}</td>`;
-                tabla += `<td> <button id="btnEditar${i}" type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#formUsuarios"><img src="../svg/pencil-square.svg" alt="edit"> Editar</button> </td>`;
-                tabla += `<td> <button id="btnEliminar${i}" type="button" class="btn btn-danger"><img src="../svg/trash3.svg" alt="edit"> Eliminar</button> </td>`;
-                tabla += "<tr>";
+                tabla += `<tr>
+                <td>${data[i].id}</td>
+                <td>${data[i].nombre}</td>
+                <td>${data[i].username}</td>
+                <td>${data[i].nivel}</td>
+                <td> <button id="btnEditar${i}" type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#formUsuarios"><img src="../svg/pencil-square.svg" alt="edit"> Editar</button> </td>
+                <td> <button id="btnEliminar${i}" type="button" class="btn btn-danger"><img src="../svg/trash3.svg" alt="edit"> Eliminar</button> </td>
+                <tr>`;
                 arrayDataUsuario[i] = i;
             }
             tablaHtml.innerHTML = tabla;
