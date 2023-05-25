4dd8ec68e85da81370f68dfa340184caee118e10
Aron <aron.cachago@gmail.com>
2023-05-24 19:27:51 -0500

    Uso de la funcion fecth para mejorar la eficencia

--- js/clientes.js
+++ js/clientes.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let tablaHtml = document.getElementById("tabla")
+    let tablaHtml = document.getElementById("tabla");
+    let modalId =  document.getElementById("modalId");
+    modalId.readOnly = true;
+    let modalNombre = document.getElementById("modalNombre");
+    let modalUsuario =  document.getElementById("modalUsuario");
+    let modalNivel = document.getElementById("modalNivel");
+    let arrayDataUsuario = [];
 
 
     fetch("../php/clientes.php")
@@ -13,16 +19,30 @@ document.addEventListener('DOMContentLoaded', function() {
             tabla += `<td>${data[i].nombre}</td>`;
             tabla += `<td>${data[i].username}</td>`;
             tabla += `<td>${data[i].nivel}</td>`;
-            tabla += `<td> <button type="button" class="btn btn-success"><i class="bi bi-pencil-square"></i> Editar</button> </td>`;
-            tabla += `<td> <button type="button" class="btn btn-danger"><i class="bi bi-trash3"></i> Eliminar</button> </td>`;
+            tabla += `<td> <button id="btnEditar${i}" type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#formUsuarios"><img src="../svg/pencil-square.svg" alt="edit"> Editar</button> </td>`;
+            tabla += `<td> <button id="btnEliminar${i}" type="button" class="btn btn-danger"><img src="../svg/trash3.svg" alt="edit"> Eliminar</button> </td>`;
             tabla += "<tr>";
+            arrayDataUsuario[i] = i;
         }
         tablaHtml.innerHTML = tabla;
-        
+
+        for (let i = 0; i < arrayDataUsuario.length; i++) {
+            document.getElementById(`btnEditar${i}`).addEventListener("click", function(){
+                datosModalEditar(i , data)
+            },false)
+        }
     })
+
     .catch(error => {
         console.error("Error al obtener los datos:", error);
     });
+
+    function datosModalEditar(id , data) {
+        modalId.value = data[id].id;
+        modalNombre.value = data[id].nombre;
+        modalUsuario.value = data[id].username;
+        modalNivel.value = data[id].nivel;
+    }
     
 
 });
